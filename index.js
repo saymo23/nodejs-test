499201712497ffd4de2be9ec67892dcd62454ad8
Daniel Santarriaga <daniel.santarriaga@gmail.com>
2020-12-15 12:11:01 -0700

    La base

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express(); 
 
 app.get('/', async (req, res) => {
-  res.send('Hello World')
+  res.send('Hello World Flex')
 });
 
 module.exports = app;
